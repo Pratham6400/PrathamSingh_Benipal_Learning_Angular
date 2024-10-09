1e9e6879680c0b298bff14b93e59a2f0418ed07c
Pratham Singh Benipal <w0837265@myscc.ca>
2024-10-09 18:02:37 -0400

    added the observable and added new service using dependency injection

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {PlayerListItemComponent} from "../player-list-item/player-list-item.component";
+import {UFCPlayersService} from "../services/ufcplayers.service";
+import {Player} from "../Shared/Modules/player";
 
 @Component({
   selector: 'app-player-list',
@@ -13,6 +15,14 @@ import {PlayerListItemComponent} from "../player-list-item/player-list-item.comp
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.css'
 })
-export class PlayerListComponent {
+export class PlayerListComponent implements OnInit {
+  userList: Player[] = [];
+  constructor(private ufcplayerService: UFCPlayersService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
 
 }
--- src/app/services/ufcplayers.service.ts
+++ src/app/services/ufcplayers.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {userList} from "../../data/mock-player";
+import {Player} from "../Shared/Modules/player";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UFCPlayersService {
-
+  private ufcplayers: Player[] = userList;
   constructor() { }
+  //adding a method that will return an Observable of the IContent array
+  // from the mock-content file in my data folder.
+  getPlayers(): Observable<Player[]> {
+    return of(userList);
+  }
 }
