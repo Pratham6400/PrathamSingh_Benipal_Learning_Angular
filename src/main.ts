fdd3b92499b35561bd5e216de381fcaac22b9f7e
Pratham Singh Benipal <w0837265@myscc.ca>
2024-10-10 16:07:35 -0400

    ensured that router is installed and setup correctly in main.ts file. Also defined a list of routes in main.ts file.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import {provideRouter, Routes} from "@angular/router";
 import {PlayerListComponent} from "./app/player-list/player-list.component";
+
 
 const routes: Routes = [
   {path:'', redirectTo: '/players', pathMatch: 'full'}, //default route
-  { path: 'players', component:  PlayerListComponent}
+  { path: 'players', component: PlayerListComponent },
 ];
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes)]
