eee56bb3e5f77fec00fea6300fa37bbfb8f5d76c
Pratham Singh Benipal <w0837265@myscc.ca>
2024-11-02 18:07:38 -0400

    started assignment-6 by testing reactive forms are installed and methods are in players.service.ts

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Player} from "../Shared/Modules/player";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-modify-list-item',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
-export class ModifyListItemComponent {
+export class ModifyListItemComponent implements OnInit {
+
+  player: Player | undefined;
+  error: string | null = null;
+
+  //making the constructor and importing form builder,form group and validators in it
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private fg: FormGroup,
+
+  ) {
+  }
+  ngOnInit(): void {
+  }
 
 }
